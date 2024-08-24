939c55e89ad93a5168d46faed56146b06d563ffa
vinay <girisaivinay1@gmail.com>
2024-08-24 14:21:58 +0530

    Save local changes before merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EbooksComponent } from './Component/ebooks/ebooks.component';
 import { AboutusComponent } from './Component/aboutus/aboutus.component';
 import { ServicesComponent } from './Component/services/services.component'; 
 import { OnlineshopComponent } from './Component/onlineshop/onlineshop.component'; 
+import { ProductComponent } from './Component/products/products.component';
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -14,6 +15,7 @@ export const appRoutes: Routes = [
   { path: 'ebooks', component: EbooksComponent },
   { path: 'aboutus', component: AboutusComponent },
   { path: 'services', component: ServicesComponent },
+  { path: 'products', component: ProductComponent},
   { path: 'onlineshop', component: OnlineshopComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home if no path is provided
   { path: '**', redirectTo: '/home' } // Redirect to home for any unknown paths
