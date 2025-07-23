dd6012b87c4089c6b2eaf20c6ec2ea9d21378380
vinay <girisaivinay1@gmail.com>
2025-07-23 14:36:37 +0530

    CORS

--- src/Backend/app.js
+++ src/Backend/app.js
@@ -8,11 +8,21 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
-
+const cors = require('cors');
 const app = express();
+const allowedOrigins = ['https://caba-pi.vercel.app'];
 
 app.use(cors({
-    origin: '*', // Not safe for production
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
 app.use(bodyParser.json());
 
